fe4dd97fed67dfae7b72ba00baed38bd9ded34d4
Hadi <hadidyeb@gmail.com>
2021-12-02 13:38:55 +0200

    set input in flex col in mobile size

--- src/Components/Social/InputField.jsx
+++ src/Components/Social/InputField.jsx
@@ -17,7 +17,7 @@ const InputField = () => {
     e.preventDefault();
     try {
       setIsSending({ isSending: true, sendingMessage: "Sending..." });
-      const result = await emailjs.send(
+      await emailjs.send(
         serviceId,
         templateId,
         {
@@ -44,21 +44,23 @@ const InputField = () => {
     <>
       <form
         onSubmit={sendMessage}
-        className="flex flex-col w-full h-auto mt-4 space-y-3 text-black md:w-1/2"
+        className="flex flex-col w-full h-auto mt-6 space-y-3 text-black md:w-1/2"
       >
-        <input
-          {...nameAttributes}
-          type="text"
-          className="sm:w-full p-2 font-medium rounded-lg outline-none dark:placeholder-[#111e27] bg-red-500 dark:bg-[#ffce45] text-white dark:text-[#111e27] placeholder-white"
-          placeholder="Your Name"
-          id="from_name"
-        />
-        <input
-          {...emailAttributes}
-          type="email"
-          className="w-full p-2 font-medium rounded-lg outline-none dark:placeholder-[#111e27] bg-red-500 dark:bg-[#ffce45] text-white dark:text-[#111e27] placeholder-white"
-          placeholder="Email Address"
-        />
+        <div className="flex flex-col items-center space-x-0 space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
+          <input
+            {...nameAttributes}
+            type="text"
+            className="sm:w-1/2 w-full p-2 font-medium rounded-lg outline-none dark:placeholder-[#111e27] bg-red-500 dark:bg-[#ffce45] text-white dark:text-[#111e27] placeholder-white"
+            placeholder="Your Name"
+            id="from_name"
+          />
+          <input
+            {...emailAttributes}
+            type="email"
+            className="sm:w-1/2 w-full p-2 font-medium rounded-lg outline-none dark:placeholder-[#111e27] bg-red-500 dark:bg-[#ffce45] text-white dark:text-[#111e27] placeholder-white"
+            placeholder="Email Address"
+          />
+        </div>
         <textarea
           name="textarea"
           cols="50"
