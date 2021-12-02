28cc4cd263ddd205f77a9e1ddb95a0386dd2774d
Hadi <hadidyeb@gmail.com>
2021-12-02 13:51:20 +0200

    set sending message

--- src/Components/Social/InputField.jsx
+++ src/Components/Social/InputField.jsx
@@ -28,16 +28,12 @@ const InputField = () => {
         },
         userId
       );
-      setTimeout(() => {
-        setIsSending({ ...isSending, sendingMessage: "Message Sent" });
-      }, 1500);
+      setIsSending({ ...isSending, sendingMessage: "Send Message" });
       setEmail("");
       setTextArea("");
       setName("");
     } catch (err) {
       alert(err);
-    } finally {
-      setIsSending({ ...isSending, sendingMessage: "Send Message" });
     }
   };
   return (
