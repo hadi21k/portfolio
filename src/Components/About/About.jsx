dd887c1ce7ff049da6548f20e1137b8d49a4fd8e
hadi21k <hadidyeb@gmail.com>
2021-12-24 20:01:26 +0200

    change scrollbar bg color

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,12 +13,12 @@ const About = () => {
         exit={{ y: 20, opacity: 0 }}
         className="container px-4 mx-auto mt-6 sm:px-16"
       >
-        <div className="py-2 font-medium notoSans text-center dark:text-[#111e27] dark:bg-white bg-red-500 text-white rounded sm:text-2xl">
-          <p className="text-sm font-bold">
+        <div className="py-2 font-medium notoSans text-center dark:text-[#111e27] dark:bg-white bg-red-500 text-white rounded">
+          <p className="text-xs font-bold sm:text-sm">
             Hello, I’m a Front End Developer based in Lebanon.
           </p>
         </div>
-        <div className=" mt-2 font-semibold text-left flex space-x-3 items-center text-[#111e27] dark:text-white sm:text-3xl">
+        <div className="mt-2 font-semibold text-left flex space-x-3 items-center text-[#111e27] dark:text-white">
           <h1 className="text-xl notoSans">Hadi Diab</h1>
         </div>
         <AboutSection />
