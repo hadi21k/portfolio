7a0f2068562ea51f7d64bba39306becccd848958
Hadi21k <hadidyeb@gmail.com>
2022-04-28 23:31:16 +0300

    redesign projects section

--- src/Components/Works/WorkSection.jsx
+++ src/Components/Works/WorkSection.jsx
@@ -1,58 +1,60 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
+import { AiOutlineGithub } from "react-icons/ai";
+import { HiOutlinePencilAlt } from "react-icons/hi";
 
 const WorkSection = () => {
-  const worksData = useSelector((state) => state.data.works);
-  return (
-    <>
-      <div className="grid grid-cols-1 gap-12 py-3 mt-6 overflow-hidden md:grid-cols-2 lg:grid-cols-3">
-        {worksData.map(({ title, sourceLink, liveDemo, image, id }) => (
-          <div key={id} className="rounded-lg overflow-hidden dark:border-white">
-            <motion.div
-              initial={{
-                opacity: 0,
-                y: -50,
-              }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.5,
-                ease: "linear",
-                delay: id * 0.5,
-              }}
-              className="cursor-pointer img"
-            >
-              <img src={image} alt={title} className="rounded-lg text-2xl" />
-            </motion.div>
-            <motion.div
-              initial={{ x: -80, opacity: 0 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, ease: "linear", delay: id * 0.5 }}
-              className="flex flex-col space-y-2 py-2"
-            >
-              <div className="text-lg text-[#111e27] dark:text-[#ffce45] font-bold">
-                <h1>{title}</h1>
-              </div>
-              <div className="flex space-x-2 itemcenter">
-                <a
-                  href={liveDemo}
-                  className="p-1 text-xs rounded dark:bg-[#ffce45] bg-red-500 dark:text-[#111e27] text-white font-bold"
-                >
-                  Live Demo
-                </a>
-                <a
-                  href={sourceLink}
-                  className="p-1 text-xs rounded dark:bg-[#ffce45] bg-red-500 dark:text-[#111e27] text-white font-bold"
-                >
-                  Source Link
-                </a>
-              </div>
-            </motion.div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+	const worksData = useSelector((state) => state.data.works);
+	return (
+		<>
+			<div className="grid grid-cols-1 gap-12 py-3 mt-6 overflow-hidden md:grid-cols-2 lg:grid-cols-3">
+				{worksData.map(({ title, sourceLink, liveDemo, image, id }) => (
+					<div
+						key={id}
+						className="overflow-hidden transition-all duration-300 transform translate-y-0 rounded-lg hover:translate-y-[-5px] group"
+					>
+						<motion.div
+							initial={{
+								opacity: 0,
+								y: -50,
+							}}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{
+								duration: 0.5,
+								ease: "linear",
+								delay: id * 0.5,
+							}}
+							className="overflow-hidden cursor-pointer img "
+						>
+							<img
+								src={image}
+								alt={title}
+								className="duration-300 rounded-lg opacity-50 group-hover:opacity-100 transtion-all"
+							/>
+						</motion.div>
+						<motion.div
+							initial={{ x: -80, opacity: 0 }}
+							animate={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.5, ease: "linear", delay: id * 0.5 }}
+							className="flex flex-row-reverse justify-between py-2"
+						>
+							<div className="flex space-x-1">
+								<a href={liveDemo}>
+									<AiOutlineGithub className="w-5 h-5 dark:text-white text-[#111e27] transition-all duration-300 transform scale-100 hover:scale-110" />
+								</a>
+								<a href={sourceLink}>
+									<HiOutlinePencilAlt className="w-5 h-5 dark:text-white text-[#111e27] transition-all duration-300 transform scale-100 hover:scale-110" />
+								</a>
+							</div>
+							<div className="text-sm text-[#111e27] dark:text-[#ffce45] font-semibold">
+								{title}
+							</div>
+						</motion.div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default WorkSection;
--- src/features/reducer.js
+++ src/features/reducer.js
@@ -7,74 +7,74 @@ import pomodoroImage from "../Assets/pomodoroTimer.webp";
 import cryptoVerse from "../Assets/cryptoverse .webp";
 
 const initialState = {
-  darkMode: false,
-  timelineItemContent: [
-    { year: "2003", description: "Born in Beirut" },
-    { year: "2020", description: "Started Front End Development" },
-    { year: "2022", description: "Learn Back End Development" },
-    { year: "2023", description: "Work Freelancer as Full Stack Development" },
-    { year: "2024", description: "Finish Computer sceince degree" },
-  ],
-  works: [
-    {
-      title: "CryptoVerse",
-      description: "A cryptocurrency market tracker",
-      sourceLink: "https://github.com/hadi21k/cryptomarket-app",
-      liveDemo: "https://coinmarket.vercel.app",
-      image: cryptoVerse,
-      id: 1,
-    },
-    {
-      title: "Motion",
-      description: "A Simple landing page for transactional data",
-      sourceLink: "https://github.com/hadi21k/Motion-website-Reactjs",
-      liveDemo: "https://motionn.netlify.app/",
-      image: motionImage,
-      id: 2,
-    },
-    {
-      title: "Dashboard App",
-      description: "Clone Dashbaord ",
-      sourceLink: "https://github.com/hadi21k/Dashboard-app-react-tailwindcss",
-      liveDemo: "https://dashboard-appp.netlify.app/",
-      image: dashboardImage,
-      id: 3,
-    },
-    {
-      title: "Tesla Clone",
-      description: "Tesla Website clone",
-      sourceLink: "https://github.com/hadi21k/Tesla-Website-Clone",
-      liveDemo: "https://teslawebsitee-clone.netlify.app/",
-      image: teslaCloneImage,
-      id: 4,
-    },
-    {
-      title: "Crypto Market",
-      description: "Crypto Website for lively prices of coins",
-      sourceLink: "https://github.com/hadi21k/crypto-market-app-reactjs",
-      liveDemo: "https://cryptomarketappp.netlify.app/",
-      image: cryptoImage,
-      id: 5,
-    },
-    {
-      title: "Pomodoro Timer",
-      description: "Pomodoro App Timer for studying using pomodoro method",
-      sourceLink: "https://github.com/hadi21k/pomodoro-timer-app-react",
-      liveDemo: "https://pomodoro-timer-appp.netlify.app/",
-      image: pomodoroImage,
-      id: 6,
-    },
-  ],
+	darkMode: false,
+	timelineItemContent: [
+		{ year: "2003", description: "Born in Beirut" },
+		{ year: "2020", description: "Started Front End Development" },
+		{ year: "2022", description: "Learn Back End Development" },
+		{ year: "2023", description: "Work Freelancer as Full Stack Development" },
+		{ year: "2024", description: "Finish Computer sceince degree" },
+	],
+	works: [
+		{
+			title: "Dashboard App",
+			description: "Clone Dashbaord ",
+			sourceLink: "https://github.com/hadi21k/Dashboard-app-react-tailwindcss",
+			liveDemo: "https://dashboard-appp.netlify.app/",
+			image: dashboardImage,
+			id: 1,
+		},
+		{
+			title: "CryptoVerse",
+			description: "A cryptocurrency market tracker",
+			sourceLink: "https://github.com/hadi21k/cryptomarket-app",
+			liveDemo: "https://coinmarket.vercel.app",
+			image: cryptoVerse,
+			id: 2,
+		},
+		{
+			title: "Motion",
+			description: "A Simple landing page for transactional data",
+			sourceLink: "https://github.com/hadi21k/Motion-website-Reactjs",
+			liveDemo: "https://motionn.netlify.app/",
+			image: motionImage,
+			id: 3,
+		},
+		{
+			title: "Tesla Clone",
+			description: "Tesla Website clone",
+			sourceLink: "https://github.com/hadi21k/Tesla-Website-Clone",
+			liveDemo: "https://teslawebsitee-clone.netlify.app/",
+			image: teslaCloneImage,
+			id: 4,
+		},
+		{
+			title: "Crypto Market",
+			description: "Crypto Website for lively prices of coins",
+			sourceLink: "https://github.com/hadi21k/crypto-market-app-reactjs",
+			liveDemo: "https://cryptomarketappp.netlify.app/",
+			image: cryptoImage,
+			id: 5,
+		},
+		{
+			title: "Pomodoro Timer",
+			description: "Pomodoro App Timer for studying using pomodoro method",
+			sourceLink: "https://github.com/hadi21k/pomodoro-timer-app-react",
+			liveDemo: "https://pomodoro-timer-appp.netlify.app/",
+			image: pomodoroImage,
+			id: 6,
+		},
+	],
 };
 
 const dataSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    setDarkMode: (state) => {
-      state.darkMode = !state.darkMode;
-    },
-  },
+	name: "counter",
+	initialState,
+	reducers: {
+		setDarkMode: (state) => {
+			state.darkMode = !state.darkMode;
+		},
+	},
 });
 
 export const { setDarkMode } = dataSlice.actions;
